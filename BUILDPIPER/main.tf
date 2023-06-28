7b6c3611a1d592db2b400f845601877f82258e86
priyanshi chauhan <priyanshi.chauhan@opstree.com>
2023-06-28 23:35:50 +0530

    openvpn

---
+++ BUILDPIPER/main.tf
@@ -0,0 +1,23 @@
+locals {
+  vpn_sg_ids     = var.vpn_sg_ids
+  public_subnet_id = var.public_subnet_id
+}
+
+module "ec2_instance" {
+  source               = "OT-CLOUD-KIT/ec2-instance/aws"
+  version              = "0.0.3"
+  count                = var.instance_count
+  ami_id               = var.ami_id
+  instance_type        = var.instance_type
+  public_ip            = var.public_ip
+  key_name             = var.key_name
+  subnet               = local.public_subnet_id
+  security_groups      = local.vpn_sg_ids
+  volume_size          = var.volume_size
+  volume_type          = var.volume_type
+  encrypted_volume     = var.encrypted_volume
+  iam_instance_profile = var.iam_instance_profile
+  ec2_name             = var.ec2_name
+  tags                 = var.tags
+}
+
---
+++ BUILDPIPER/vars.tf
@@ -0,0 +1,84 @@
+variable "region" {
+  type        = string
+  description = "Region where resource will be created."
+}
+
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  type        = number
+  default     = 1
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instances"
+  type        = string
+  default     = "ami-12345678"
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2 instances"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "public_ip" {
+  description = "Whether to assign a public IP address to the EC2 instances"
+  type        = bool
+  default     = false
+}
+
+variable "key_name" {
+  description = "Name of the key pair to associate with the EC2 instances"
+  type        = string
+  default     = ""
+}
+
+variable "public_subnet_id" {
+  description = "ID of the subnet for the EC2 instances"
+  type        = string
+  default     = "subnet-12345678"
+}
+
+variable "vpn_sg_ids" {
+  description = "List of security group IDs for the EC2 instances"
+  type        = list(string)
+  default     = []
+}
+
+variable "volume_size" {
+  description = "Size of the root volume for the EC2 instances (in GB)"
+  type        = number
+  default     = 100
+}
+
+variable "volume_type" {
+  description = "Type of the root volume for the EC2 instances"
+  type        = string
+  default     = "gp2"
+}
+
+variable "encrypted_volume" {
+  description = "Whether the root volume should be encrypted"
+  type        = bool
+  default     = false
+}
+
+variable "iam_instance_profile" {
+  description = "IAM instance profile for the EC2 instances"
+  type        = string
+  default     = ""
+}
+
+variable "ec2_name" {
+  description = "Name for the EC2 instances"
+  type        = string
+  default     = "my-ec2-instance"
+}
+
+variable "tags" {
+  description = "Additional tags for the EC2 instances"
+  type        = map(string)
+  default     = {}
+}
+
+
